924fce0b87476c79b580ac977650daaff1beadc1
jordanwanlass <jwanlass8@gmail.com>
2025-06-12 18:12:53 -0600

    can upload a file and display its contents in a table

--- src/components/createDataSet.tsx
+++ src/components/createDataSet.tsx
@@ -1,23 +1,62 @@
 import { useState } from "react";
 import { Input } from "./ui/input";
+import parse from "papaparse";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
 
 export default function CreateDataSet() {
   const [file, setFile] = useState<any>(null);
+  const [dataSet, setDataSet] = useState<any[]>([]);
 
   const handFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+    const inputedFile = event.target.files?.[0];
 
-    if (file) {
-      setFile(file);
+    if (inputedFile) {
+      console.log(inputedFile.webkitRelativePath);
+      parse.parse(inputedFile, {
+        step: function (result) {
+          dataSet.push(result.data);
+        },
+        complete: function (results, file) {
+          console.log("complete");
+          setFile(file);
+        },
+      });
     } else {
       console.log("No file selected");
     }
   };
 
   return (
-    <div className="h-full grid items-center justify-center">
+    <div className={`h-full grid ${file ? "py-4 px-4" : "items-center justify-center"}`}>
       {file ? (
-        <>{file.name}</>
+        <div>
+          <div>Row above that has options (add data)</div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                {dataSet[0].map((header: string, index: number) => (
+                  <TableHead key={index}>{header}</TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {dataSet.slice(1).map((row, index) => (
+                <TableRow>
+                  {row.map((cell: any, cellIndex: number) => (
+                    <TableCell key={cellIndex}>{cell}</TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       ) : (
         <div className="grid w-full max-w-sm items-center gap-3">
           <div>Upload your file to begin defining your data set.</div>
--- src/page.tsx
+++ src/page.tsx
@@ -20,19 +20,21 @@ const links = [
     to: "/dataSets",
     icon: <List className="h-4 w-4" />,
   },
-
 ];
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-4 px-4 py-4 m-0" style={{ minHeight: "100vh"}}>
+    <div
+      className="grid grid-cols-4 px-4 py-4 m-0"
+      style={{ minHeight: "100vh" }}
+    >
       <HashRouter>
         <div className="flex flex-col mr-4">
           <div className="mb-10 flex items-center gap-2 px-1 italic font-semibold">
             <SquareStack className="h-4 w-4" />
             Easy Data Viz
           </div>
-          
+
           {links.map((link) => {
             return (
               <Link
@@ -45,12 +47,12 @@ export default function Page() {
             );
           })}
         </div>
-        <div className="col-span-3 border rounded-2xl shadow-xl">
-        <Routes>
-          <Route path="/createDataSet" element={<CreateDataSet />} />
-          <Route path="/analyzeDataSet" element={<AnalyzeDataSet />} />
-          <Route path="/dataSets" element={<DataSets />} />
-        </Routes>
+        <div className="col-span-3 border rounded-2xl shadow-xl/20 overflow-scroll" style={{ maxHeight: "95vh" }}>
+          <Routes>
+            <Route path="/createDataSet" element={<CreateDataSet />} />
+            <Route path="/analyzeDataSet" element={<AnalyzeDataSet />} />
+            <Route path="/dataSets" element={<DataSets />} />
+          </Routes>
         </div>
       </HashRouter>
     </div>
